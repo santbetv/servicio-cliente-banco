6aa31aefc9ab901106b16b83ad69d49cdc949b44
santbetv <santiago-betancur@hotmail.com>
2023-10-16 21:46:37 -0500

    feat agregando proyecto principal cliente persona

---
+++ src/main/java/com/devsu/serviciocliente/ServicioclienteApplication.java
@@ -0,0 +1,13 @@
+package com.devsu.serviciocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioclienteApplication.class, args);
+	}
+
+}
